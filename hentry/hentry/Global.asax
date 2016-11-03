8e4f4678ed6463403c9e534370ea57aad3c71a35
laurima <H9142@student.jamk.fi>
2016-11-03 09:41:00 +0200

    Initial commit

---
+++ hentry/hentry/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hentry.MvcApplication" Language="C#" %>
---
+++ hentry/hentry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hentry.Startup))]
+namespace hentry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
