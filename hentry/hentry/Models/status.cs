970601d3e3df25bdce3836f4ff4627ba479377e2
Sami <H8543@student.jamk.fi>
2016-12-04 04:43:00 +0200

    Fixes display valuef for status field

--- hentry/hentry/Models/status.cs
+++ hentry/hentry/Models/status.cs
@@ -11,7 +11,8 @@ namespace hentry.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class status
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,7 @@ namespace hentry.Models
         }
     
         public int id { get; set; }
+        [Display(Name = "Status")]
         public string status1 { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
